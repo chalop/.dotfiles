6e81159de6e427a02d25728703cf3a9eab7abbbc
chalop <contact@chalop.dev>
2023-03-20 15:17:45 -0400

    add change window focus

---
+++ skhd/.config/skhd/fullscreen.sh
@@ -0,0 +1,10 @@
+yabai -m space --toggle padding
+yabai -m space --toggle gap 
+yabai -m config external_bar off:0:0
+scketchybar --bar hidden=true
+
+yabai -m space --toggle padding
+yabai -m space --toggle  gag 
+yabai -m config external_bar all:20:0
+scketchybar --bar hidden=false
+
